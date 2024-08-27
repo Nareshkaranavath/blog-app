9b5dfad9c707b0fa33f5e928ea78512b2242e9c5
Nareshkaranavath <140869848+Nareshkaranavath@users.noreply.github.com>
2024-08-27 18:29:02 +0530

    Delete Documents/workspace-spring-tool-suite-4-4.21.0.RELEASE/blog-app directory

--- Documents/workspace-spring-tool-suite-4-4.21.0.RELEASE/blog-app/src/main/java/com/blogapp_api/repositories/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.blogapp_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blogapp_api.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-
-}
